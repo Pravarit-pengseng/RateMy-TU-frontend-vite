47d949e3e0be0ce17a18818991b357274e898bf6
Pravarit-pengseng <pravarit947@gmail.com>
2025-10-07 15:51:38 +0700

    fixed scroll, home pic
    by pham

--- src/Components/Pages/User/HomepageUser.jsx
+++ src/Components/Pages/User/HomepageUser.jsx
@@ -8,6 +8,7 @@ import { getPopularSearches } from "../../../Function/search";
 //icon
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
 function HomepageUser() {
+  const API = import.meta.env.REACT_APP_API;
   const role = useSelector((state) => state.user?.user?.role);
   const [subjects, setSubjects] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -32,10 +33,9 @@ function HomepageUser() {
   }, []);
 
   return (
-    <div className="bg-[#2d2f3b] min-h-screen w-full overflow-x-hidden">
+    <div className="bg-[#2d2f3b] h-screen w-screen overflow-y-scroll">
       <div className="w-full">
         {/* Hero */}
-<<<<<<< HEAD
         <div className=" ">
           <div className="bg-[url('/assets/home-bg.png')] bg-cover bg-center bg-no-repeat w-full min-h-[424px] py-12 sm:py-12">
            <div className="text-center">
@@ -47,19 +47,6 @@ function HomepageUser() {
               academic choice
             </p>
            </div>
-=======
-        <div>
-          <div className="bg-[url('/assets/home-bg.png')] bg-no-repeat bg-center bg-cover w-full h-[calc(100vh-150px)] flex flex-col items-center justify-center">
-            <div className="text-center">
-              <h1 className="text-white font-bold mt-1 sm:mt-8 mb-1 sm:mb-8 text-[32px] sm:text-[48px] md:text-[64px] lg:text-[80px]">
-                Find your <ins>FAVORITE</ins> subject
-              </h1>
-              <p className="text-white mb-3 sm:mb-5 px-2 sm:px-10">
-                Explore friends review, rating and professor to make informed
-                academic choice
-              </p>
-            </div>
->>>>>>> parent of 8b7d675 (Revert "fixed scroll ,home picture")
             <div className="flex justify-center mb-4 sm:mb-8">
               <Search />
             </div>
