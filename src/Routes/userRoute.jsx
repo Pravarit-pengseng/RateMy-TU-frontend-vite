a38dd65ed0a20f06f9b18cd8c1ee5a1201144db7
Pravarit-pengseng <pravarit947@gmail.com>
2025-10-14 00:46:04 +0700

    Update userRoute.jsx

--- src/Routes/userRoute.jsx
+++ src/Routes/userRoute.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import HeaderBar from "../Layout/headerBar";
-import NotFound404 from "../components/Pages/NotFound404";
+import NotFound404 from "../Components/Pages/NotFound404";
 
 const UserRoute = ({ children }) => {
   const { user } = useSelector((state) => ({ ...state }));
